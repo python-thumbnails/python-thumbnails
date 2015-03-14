df92086873409b0c3ec42bc451b9d45a58bd828f
Rolf Erik Lekang <me@rolflekang.com>
2015-03-14 23:51:21 +0100

    Fix bug in SettingsWrapper

--- thumbnails/conf/wrapper.py
+++ thumbnails/conf/wrapper.py
@@ -9,19 +9,19 @@ class SettingsWrapper(object):
 
     def __init__(self):
         self._load_from_module(defaults, self.defaults)
-        for setting in dir(defaults):
-            setattr(self.defaults, setting, getattr(defaults, setting))
-
-        self._load_django_settings()
 
     def __getattr__(self, key):
         value = self.defaults.get(key, None)
 
         if value is None:
-            raise AttributeError()
+            raise AttributeError('No setting for "{}".'.format(key))
 
         return value
 
     def _load_from_module(self, _module, target):
         for setting in dir(_module):
-            setattr(target, setting, getattr(_module, setting))
+            if not setting.startswith('_'):
+                if isinstance(target, dict):
+                    target[setting] = getattr(_module, setting)
+                else:
+                    setattr(target, setting, getattr(_module, setting))
