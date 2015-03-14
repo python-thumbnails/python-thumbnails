807b80b9d28bc5fe3629e85a348302a175a13360
Rolf Erik Lekang <me@rolflekang.com>
2015-03-14 19:25:52 +0100

    Add thumbnails.get_thumbnail()

--- thumbnails/__init__.py
+++ thumbnails/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
+from thumbnails import engines
 
 __version__ = '0.1.0'
+
+
+def get_thumbnail(*args, **kwargs):
+    return engines.get_current_engine().get_thumbnail(*args, **kwargs)
---
+++ thumbnails/engines/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
+def get_current_engine():
+    return None
