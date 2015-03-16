393ae40eeb6d0b0a402e49f5b2309a017d3bbe92
Rolf Erik Lekang <me@rolflekang.com>
2015-03-16 11:21:52 +0100

    Add pillow engine

--- tests/test_engines.py
+++ tests/test_engines.py
@@ -3,43 +3,67 @@ import os
 import unittest
 
 from PIL import Image
+from thumbnails.compat import BytesIO
 
 from thumbnails.engines.base import ThumbnailBaseEngine
 from thumbnails.engines.dummy import DummmyEngine
+from thumbnails.engines.pillow import PillowEngine
+from thumbnails.images import SourceFile
 
 
 class EngineTestMixin(object):
-    engine = None
 
     def setUp(self):
         self.engine = self.ENGINE()
         self.filename = os.path.join(os.path.dirname(__file__), 'test_image.jpg')
+        self.file = SourceFile(self.filename)
+        self.url = SourceFile('http://puppies.lkng.me/400x600/')
+
         image = Image.new('L', (400, 600))
         image.save(self.filename)
 
     def tearDown(self):
         os.remove(self.filename)
 
-    def test_create(self):
-        thumbnail = self.engine.create(self.filename, (200, 300), None)
-        self.assertEqual(thumbnail.width, 200)
-        self.assertEqual(thumbnail.height, 300)
-        self.assertEqual(thumbnail.url, '')
+    @unittest.skip('Awaiting')
+    def test_create_from_file(self):
+        thumbnail = self.engine.create(self.file, (200, 300), None)
+        self.assertEqual(thumbnail.size[0], 200)
+        self.assertEqual(thumbnail.size[1], 300)
+
+    def test_create_from_url(self):
+        thumbnail = self.engine.create(self.url, (200, 300), None)
+        self.assertEqual(thumbnail.size[0], 200)
+        self.assertEqual(thumbnail.size[1], 300)
 
 
 class BaseEngineTestCase(EngineTestMixin, unittest.TestCase):
     ENGINE = ThumbnailBaseEngine
 
-    def test_create(self):
+    def test_create_from_file(self):
         with self.assertRaises(NotImplementedError):
-            self.engine.create(self.filename, (200, 300), None)
+            self.engine.create(self.file, (200, 300), None)
+
+    def test_create_from_url(self):
+        with self.assertRaises(NotImplementedError):
+            self.engine.create(self.url, (200, 300), None)
+
+
+class PillowEngineTestCase(EngineTestMixin, unittest.TestCase):
+    ENGINE = PillowEngine
 
 
 class DummyEngineTestCase(EngineTestMixin, unittest.TestCase):
     ENGINE = DummmyEngine
 
-    def test_create(self):
-        thumbnail = self.engine.create(self.filename, (200, 300), None)
+    def test_create_from_file(self):
+        thumbnail = self.engine.create(self.file, (200, 300), None)
+        self.assertEqual(thumbnail.width, 200)
+        self.assertEqual(thumbnail.height, 300)
+        self.assertEqual(thumbnail.url, 'http://puppies.lkng.me/200x300')
+
+    def test_create_from_url(self):
+        thumbnail = self.engine.create(self.url, (200, 300), None)
         self.assertEqual(thumbnail.width, 200)
         self.assertEqual(thumbnail.height, 300)
         self.assertEqual(thumbnail.url, 'http://puppies.lkng.me/200x300')
---
+++ thumbnails/engines/pillow.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from thumbnails.compat import BytesIO
+
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
+from thumbnails.engines.base import ThumbnailBaseEngine
+
+
+class PillowEngine(ThumbnailBaseEngine):
+
+    def engine_load_image(self, original):
+        return Image.open(BytesIO(original.open().read()))
+
+    def engine_save_image(self, image, location):
+        image.save(location)
+
+    def get_image_size(self, image):
+        return image.size
+
+    def get_image_info(self, image):
+        return image.info or {}
+
+    def engine_scale(self, image, width, height):
+        return image.resize((width, height), resample=Image.ANTIALIAS)
+
+    def engine_crop(self, image, size, crop, options):
+        x, y = crop
+        width, height = size
+        return image.crop((x, y, x + width, y + height))
