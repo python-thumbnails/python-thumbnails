a5cd95fc2c2588c9a963a2c6a09331c30bba976c
Rolf Erik Lekang <me@rolflekang.com>
2015-03-18 22:07:01 +0100

    Change django thumbnails path

--- thumbnails/conf/defaults_django.py
+++ thumbnails/conf/defaults_django.py
@@ -3,6 +3,6 @@ import os
 
 from django.conf import settings
 
-THUMBNAIL_PATH = os.path.join(settings.MEDIA_ROOT, 'thumbnails')
-THUMBNAIL_URL = settings.MEDIA_URL + 'thumbnails'
+THUMBNAIL_PATH = os.path.join(settings.MEDIA_ROOT, 'thumbnails-cache')
+THUMBNAIL_URL = settings.MEDIA_URL + 'thumbnails-cache'
 THUMBNAIL_CACHE_BACKEND = 'thumbnails.cache_backends.DjangoCacheBackend'
