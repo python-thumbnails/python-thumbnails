13b2da8afb34495ba433bbef8094dd985fab35e0
Rolf Erik Lekang <me@rolflekang.com>
2015-03-20 08:49:26 +0100

    Add documentation for cache-backends

--- docs/cache-backends.rst
+++ docs/cache-backends.rst
@@ -1,6 +1,18 @@
 Cache backends
 --------------
 
+The cache backends is used to store references to already created thumbnails in order to
+avoid unnecessary disk or network usage. The ``Thumbnail`` object is cached and will be returned
+directly in ``get_thumbnail`` if the cache returns it.
+
 .. autoclass:: thumbnails.cache_backends.BaseCacheBackend
+    :members:
+    :private-members:
+
 .. autoclass:: thumbnails.cache_backends.SimpleCacheBackend
+    :members:
+    :private-members:
+
 .. autoclass:: thumbnails.cache_backends.DjangoCacheBackend
+    :members:
+    :private-members:
--- thumbnails/cache_backends.py
+++ thumbnails/cache_backends.py
@@ -2,23 +2,51 @@
 
 
 class BaseCacheBackend(object):
+    """
+    Extendible cache backend that should be used when creating a new cache backend. Subclasses
+    should only override methods prefixed with ``_``.
+    """
 
     def get(self, thumbnail_name):
+        """
+        Wrapper for ``_get``, which converts the thumbnail_name to String if necessary before
+        calling ``_get``
+
+        :rtype: Thumbnail
+        """
         if isinstance(thumbnail_name, list):
             thumbnail_name = ''.join(thumbnail_name)
         return self._get(thumbnail_name)
 
     def set(self, thumbnail):
+        """
+        Wrapper for ``_set``.
+        """
         return self._set(thumbnail.name, thumbnail)
 
     def _get(self, thumbnail_name):
+        """
+        Backend specific handling of get, should be overridden by subclasses.
+
+        :param thumbnail_name: String or list with the name/hash of the thumbnail.
+        :rtype: Thumbnail
+        """
         raise NotImplementedError
 
     def _set(self, thumbnail_name, thumbnail):
+        """
+        Backend specific handling of set, should be overridden by subclasses.
+
+        :param thumbnail_name: String with the name of the thumbnail.
+        :param thumbnail: The Thumbnail object that should be cached.
+        """
         raise NotImplementedError
 
 
 class SimpleCacheBackend(BaseCacheBackend):
+    """
+    Cache backend that stores objects in a dict on the backend instance.
+    """
 
     thumbnails = {}
 
@@ -31,6 +59,9 @@ class SimpleCacheBackend(BaseCacheBackend):
 
 
 class DjangoCacheBackend(BaseCacheBackend):
+    """
+    Cache backend that uses Django's cache.
+    """
 
     def __init__(self):
         from django.core.cache import cache  # noqa isort:skip
