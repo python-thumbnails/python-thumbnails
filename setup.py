ba2dd66b46eccb07bf4a162784dd894ac56114c1
Rolf Erik Lekang <me@rolflekang.com>
2015-03-22 15:37:04 +0100

    Add beta classifier

--- setup.py
+++ setup.py
@@ -28,7 +28,8 @@ setup(
     ],
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python",
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
     ]
