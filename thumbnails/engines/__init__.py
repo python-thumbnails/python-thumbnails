575d971a61483f0a31e66a1fdd3565b5c020f0e4
Rolf Erik Lekang <me@rolflekang.com>
2015-03-24 16:44:50 +0100

    Add wand engine
    Fixes #4

--- thumbnails/engines/__init__.py
+++ thumbnails/engines/__init__.py
@@ -2,3 +2,7 @@
 from thumbnails.engines.base import BaseThumbnailEngine  # noqa
 from thumbnails.engines.pillow import PillowEngine  # noqa
 from thumbnails.engines.dummy import DummyEngine  # noqa
+try:
+    from thumbnails.engines.wand import WandEngine  # noqa
+except ImportError:
+    WandEngine = None
