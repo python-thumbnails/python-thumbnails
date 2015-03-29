9c0c7ee84bb234f4569c2c3ff9150f6fe8d4b0ab
Rolf Erik Lekang <me@rolflekang.com>
2015-03-29 10:14:08 +0200

    Add django filters for markdown and html

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+from thumbnails import settings
+
 from .compat import mock
 from .utils import has_installed
 
@@ -55,3 +57,55 @@ class TemplateTagTestCase(unittest.TestCase):
         }))
 
         mock_get_thumbnail.assert_called_once_with(_file, '200x200')
+
+
+@mock.patch('{}.get_thumbnail'.format(settings.THUMBNAIL_ENGINE), lambda *args: None)
+@mock.patch('thumbnails.images.Thumbnail.save', lambda *args: None)
+@mock.patch('thumbnails.images.Thumbnail.save_alternative_resolution', lambda *args: None)
+@unittest.skipIf(not has_installed('django'), 'Django not installed')
+class FilterTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.MARKDOWN_TEMPLATE = Template('{% load thumbnails %}{{ text|markdown_thumbnails }}')
+        self.HTML_TEMPLATE = Template('{% load thumbnails %}{{ text|html_thumbnails }}')
+        self.SAFE_HTML_TEMPLATE = Template('{% load thumbnails %}{{ text|safe_html_thumbnails }}')
+
+    def test_markdown_filter(self):
+        self.assertEqual(self.MARKDOWN_TEMPLATE.render(Context({
+            'text': 'image: ![Title](/image.jpg)'
+        })), 'image: ![Title](thumbnails-cache/09a/e5965fd6bcafba14e3e696059acfa3db6ca8e.jpg)')
+
+        self.assertEqual(self.MARKDOWN_TEMPLATE.render(Context({
+            'text': 'image: ![](/image.jpg)'
+        })), 'image: ![](thumbnails-cache/09a/e5965fd6bcafba14e3e696059acfa3db6ca8e.jpg)')
+
+    def test_html_filter(self):
+        self.assertEqual(self.HTML_TEMPLATE.render(Context({
+            'text': 'image: <img src="/image.jpg" alt="Title" />'
+        })), 'image: <img src="thumbnails-cache/09a/e5965fd6bcafba14e3e696059acfa3db6ca8e.jpg"'
+             ' alt="Title" />')
+
+        self.assertEqual(self.HTML_TEMPLATE.render(Context({
+            'text': 'image: <img src="/image.jpg" alt="" />'
+        })), 'image: <img src="thumbnails-cache/09a/e5965fd6bcafba14e3e696059acfa3db6ca8e.jpg"'
+             ' alt="" />')
+
+        self.assertEqual(self.HTML_TEMPLATE.render(Context({
+            'text': 'image: <img src="/image.jpg" />'
+        })), 'image: <img src="thumbnails-cache/09a/e5965fd6bcafba14e3e696059acfa3db6ca8e.jpg" />')
+
+    def test_safe_html_filter(self):
+        self.assertEqual(self.SAFE_HTML_TEMPLATE.render(Context({
+            'text': 'image: <img src="/image.jpg" alt="Title" />'
+        })), 'image: &lt;img src=&quot;thumbnails-cache/09a/e5965fd6bcafba14e3e696059acfa3db6ca8e'
+             '.jpg&quot; alt=&quot;Title&quot; /&gt;')
+
+        self.assertEqual(self.SAFE_HTML_TEMPLATE.render(Context({
+            'text': 'image: <img src="/image.jpg" alt="" />'
+        })), 'image: &lt;img src=&quot;thumbnails-cache/09a/e5965fd6bcafba14e3e696059acfa3db6ca8e'
+             '.jpg&quot; alt=&quot;&quot; /&gt;')
+
+        self.assertEqual(self.SAFE_HTML_TEMPLATE.render(Context({
+            'text': 'image: <img src="/image.jpg" />'
+        })), 'image: &lt;img src=&quot;thumbnails-cache/09a/e5965fd6bcafba14e3e696059acfa3db6ca8e'
+             '.jpg&quot; /&gt;')
