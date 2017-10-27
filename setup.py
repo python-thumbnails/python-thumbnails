12b72212d239fe513af918abb7e92005eaea79d8
Rolf Erik Lekang <me@rolflekang.com>
2017-10-27 22:53:47 +0200

    fix: Change dependencies to use range instead of ==
    Fixes #29

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ setup(
     packages=find_packages(exclude='tests'),
     license='MIT',
     install_requires=[
-        'six==1.9.0',
-        'requests==2.7.0'
+        'six>=1.9.0',
+        'requests>=2.7.0'
     ],
     include_package_data=True,
     classifiers=[
